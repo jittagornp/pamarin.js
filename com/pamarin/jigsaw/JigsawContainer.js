3e4ba9d07b0a2d4783e5ac22ddec9dad4e9ebd03
jittagorn pitakmetagoon <jittagornp@gmail.com>
2014-07-03 02:02:50 +0700

    Update JigsawContainer.js

--- com/pamarin/jigsaw/JigsawContainer.js
+++ com/pamarin/jigsaw/JigsawContainer.js
@@ -12,8 +12,10 @@ define('com.pamarin.jigsaw.JigsawContainer', [
     'com.pamarin.jigsaw.JigsawManager',
     'com.pamarin.core.logging.LoggerFactory',
     'com.pamarin.core.creational.Singleton',
-    'com.pamarin.core.util.StringUtils'
-], function(module, Class, PageContext, JigsawLoader, PathFilter, $, JigsawManager, LoggerFactory, Singleton, StringUtils) {
+    'com.pamarin.core.util.StringUtils',
+    'com.pamarin.jigsaw.JigsawContextBean',
+    'com.pamarin.core.exception.IllegalStateException'
+], function(module, Class, PageContext, JigsawLoader, PathFilter, $, JigsawManager, LoggerFactory, Singleton, StringUtils, JigsawContextBean, IllegalStateException) {
 
     /**
      * @class JigsawContainer
@@ -21,136 +23,177 @@ define('com.pamarin.jigsaw.JigsawContainer', [
     var JigsawContainer = Class.define(module.id, (function() {
 
         var LOG = LoggerFactory.getLogger(module.id);
-        var CONTEXT_SCOPED = ['page', 'tab', 'section'];
 
-        var jigsawManager = JigsawManager.getInstance();
-        var pageContext = PageContext.getInstance();
-        var contextQueue = {};
-        var started = true;
+        var SCOPEDS = JigsawContextBean.constant('SCOPEDS');
+        var PAGE_SCOPED = JigsawContextBean.constant('PAGE_SCOPED');
+        var TAB_SCOPED = JigsawContextBean.constant('TAB_SCOPED');
+        var SECTION_SCOPED = JigsawContextBean.constant('SECTION_SCOPED');
 
-        function log(contextScoped, contextBean, loadPath) {
-            LOG.debug('scoped --> {}', contextScoped);
-            loadPath && LOG.debug('loadPath --> {}', loadPath);
-            LOG.debug('contextBean --> {}', contextBean);
-            LOG.debug('callerQueue --> {}', contextQueue);
-        }
+        var started = false;
 
-        function concatQuerystring(context) {
-            var querystring = context.getQuerystring();
-            do {
-                var child = context.getChild();
-                if (child !== null) {
-                    querystring = querystring + '&'
-                            + context.getChildContextAttribute() + '='
-                            + child.getName();
+        return {
+            /**/
+            variable: {
+                contextQueue_: null,
+                started_: true,
+                pageContext_: null,
+                jigsawManager_: null
+            },
+            /**/
+            constructor: function() {
+                throw new IllegalStateException('can\'t initialize \'{}\' instance by new operation.', module.id);
+            },
+            /**
+             * @param {ContextBean} contextBean
+             * @returns {String}
+             */
+            concatQuerystring: function(context) {
+                var querystring = context.getQuerystring();
+                do {
+                    var child = context.getChild();
+                    if (child !== null) {
+                        querystring = querystring + '&'
+                                + context.getChildContextAttribute() + '='
+                                + child.getName();
+                    }
+
+                    context = child;
+                } while (context !== null);
+
+                if (querystring[0] === '&') {
+                    querystring = querystring.substr(1);
                 }
 
-                context = child;
-            } while (context !== null);
+                return querystring;
+            },
+            /**
+             * @param {String} contextScoped
+             * @param {ContextBean} contextBean
+             * @param {String} loadPath 
+             */
+            log: function(contextScoped, contextBean, loadPath) {
+                LOG.debug('scoped --> {}', contextScoped);
+                loadPath && LOG.debug('loadPath --> {}', loadPath);
+                LOG.debug('contextBean --> {}', contextBean);
+                LOG.debug('callerQueue --> {}', this.contextQueue_);
+            },
+            /**
+             * @param {ContextBean} contextBean
+             * @returns {String} 
+             */
+            generatePath: function(context) {
+                var path = PathFilter.filter(context.getContextPath());
+                return path + (StringUtils.found(path, '?') ? '&' : '?')
+                        + this.concatQuerystring(context);
+            },
+            /**
+             * @param {String} scoped
+             * @param {Object} 
+             */
+            popQueue: function(scoped) {
+                var index = SCOPEDS.indexOf(scoped);
+                var sc = SCOPEDS[index + 1];
+                var ctx = this.contextQueue_[sc];
+                delete this.contextQueue_[sc];
+                return ctx;
+            },
+            /**
+             * @param {String} contextScoped
+             * @param {ContextBean} contextBean
+             */
+            startController: function(contextScoped, contextBean) {
+                var that = this;
+                var scopedName = '[data-scoped=' + contextScoped + ']';
+                var $context = $(scopedName);
+                var hasContext = $context.length > 0;
 
-            if (querystring[0] === '&') {
-                querystring = querystring.substr(1);
-            }
-
-            return querystring;
-        }
-
-        function generatePath(context) {
-            var path = PathFilter.filter(context.getContextPath());
-            return path + (StringUtils.found(path, '?') ? '&' : '?')
-                    + concatQuerystring(context);
-        }
-
-        function popQueue(scoped) {
-            var index = CONTEXT_SCOPED.indexOf(scoped);
-            var q = CONTEXT_SCOPED[index + 1];
-            var ctx = contextQueue[q];
-            delete contextQueue[q];
-            return ctx;
-        }
-
-        function startController(contextScoped, contextBean) {
-            var scopedName = '[data-scoped=' + contextScoped + ']';
-            var $context = $(scopedName);
-            var hasContext = $context.length > 0;
-
-            if (!hasContext) {
-                popQueue(contextScoped);
-                started = true;
-                log(contextScoped, contextBean);
-                return;
-            }
-
-            var contextLoaded = $context.attr('data-state') === 'loaded';
-            if (contextLoaded) {
-                jigsawManager.startControllerByScoped(contextScoped);
-
-                $context.attr('data-state', 'unload');
-                popQueue(contextScoped);
-                started = true;
-                log(contextScoped, contextBean);
-                return;
-            }
-
-            //lazy load context environment
-            var loadPath = generatePath(contextBean);
-            JigsawLoader.load(scopedName, loadPath, function() {
-                jigsawManager.startControllerByScoped(contextScoped);
-                var ctx = popQueue(contextScoped);
-                if (!ctx) {
-                    started = true;
+                if (!hasContext) {
+                    this.popQueue(contextScoped);
+                    this.started_ = true;
+                    this.log(contextScoped, contextBean);
                     return;
                 }
 
-                startController(ctx.scoped, ctx.context);
-            });
-            
-            started = false;
-            log(contextScoped, contextBean, loadPath);
-        }
+                var contextLoaded = $context.attr('data-state') === 'loaded';
+                if (contextLoaded) {
+                    this.jigsawManager_.startControllerByScoped(contextScoped);
 
-        function waitInQueue(contextScoped, contextBean) {
-            contextQueue[contextScoped] = {
-                scoped: contextScoped,
-                context: contextBean
-            };
-        }
+                    $context.attr('data-state', 'unload');
+                    this.popQueue(contextScoped);
+                    this.started_ = true;
+                    this.log(contextScoped, contextBean);
+                    return;
+                }
 
-        function restartController(contextScoped, contextBean) {
-            jigsawManager.stopControllerByScoped(contextScoped);
-            if (started) {
-                startController(contextScoped, contextBean);
-                return;
-            }
+                //lazy load context environment
+                var loadPath = this.generatePath(contextBean);
+                JigsawLoader.load(scopedName, loadPath, function() {
+                    that.jigsawManager_.startControllerByScoped(contextScoped);
+                    var ctx = that.popQueue(contextScoped);
+                    if (!ctx) {
+                        that.started_ = true;
+                        return;
+                    }
 
-            waitInQueue(contextScoped, contextBean);
-        }
+                    that.startController(ctx.scoped, ctx.context);
+                });
 
-        function startContextListener() {
-            pageContext.addPageChangeListener(function(contextBean) {
-                restartController('page', contextBean);
-            }, false);
+                this.started_ = false;
+                this.log(contextScoped, contextBean, loadPath);
+            },
+            /**
+             * @param {String} contextScoped
+             * @param {ContextBean} contextBean
+             */
+            restartController: function(contextScoped, contextBean) {
+                this.jigsawManager_.stopControllerByScoped(contextScoped);
+                if (this.started_) {
+                    this.startController(contextScoped, contextBean);
+                    return;
+                }
 
-            pageContext.addTabChangeListener(function(contextBean) {
-                restartController('tab', contextBean);
-            }, false);
+                this.waitInQueue(contextScoped, contextBean);
+            },
+            /**
+             * @param {String} contextScoped
+             * @param {ContextBean} contextBean
+             */
+            waitInQueue: function(contextScoped, contextBean) {
+                this.contextQueue_[contextScoped] = {
+                    scoped: contextScoped,
+                    context: contextBean
+                };
+            },
+            /**/
+            start: function() {
+                var that = this;
+                this.contextQueue_ = [];
 
-            pageContext.addSectionChangeListener(function(contextBean) {
-                restartController('section', contextBean);
-            }, false);
-        }
+                if (!started) {
+                    started = true;
 
-        return {
+                    this.jigsawManager_ = JigsawManager.getInstance();
+                    this.pageContext_ = PageContext.getInstance();
+
+                    this.pageContext_.addPageChangeListener(function(contextBean) {
+                        that.restartController(PAGE_SCOPED, contextBean);
+                    }, false);
+
+                    this.pageContext_.addTabChangeListener(function(contextBean) {
+                        that.restartController(TAB_SCOPED, contextBean);
+                    }, false);
+
+                    this.pageContext_.addSectionChangeListener(function(contextBean) {
+                        that.restartController(SECTION_SCOPED, contextBean);
+                    }, false);
+                }
+            },
             /**/
             static: {
                 /**/
                 getInstance: function() {
                     return Singleton.getInstance(JigsawContainer);
                 }
-            },
-            /**/
-            start: function() {
-                startContextListener();
             }
         };
     })());
